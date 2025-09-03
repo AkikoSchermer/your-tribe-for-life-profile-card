78c37d019f653aa01aa32b5e792f6c6ba205359e
Akikosophia <asschermer@hotmail.com>
2025-09-03 20:34:17 +0200

    Data opgehaald & getoond
    - Het ophalen van alle studenten en het tonen op id

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+
+let { data } = $props();
+
+const members = data.members.data;
+
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+{#each members as member}
+<a href="/{member.id}">
+    {member.name} - {member.bio} 
+</a><br>
+{/each}
---
+++ my-app/src/routes/[id]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    let { data } = $props();
+   
+   const member = data.member;
+
+</script> 
+   
+  <h1>{member.name}</h1>
