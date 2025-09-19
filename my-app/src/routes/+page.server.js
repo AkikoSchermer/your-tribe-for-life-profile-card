3b96fceb8a966872307aa112fd74c0a52326e03c
Akikosophia <asschermer@hotmail.com>
2025-09-19 11:34:08 +0200

    #3 refactor(home): move personal page content from [id] to home
    Moved all HTML, styling, and structure from previous [id] page to routes/+page.svelte
    
    Home now directly displays personal information

--- my-app/src/routes/+page.server.js
+++ my-app/src/routes/+page.server.js
@@ -1,6 +1,4 @@
 export async function load  ({ url }) {
-// const membersResponse = await fetch (`https://fdnd.directus.app/items/person?fields=*,squads.squad_id.name,squads.squad_id.cohort,squads.squad_id.tribe.name&filter[squads][squad_id][cohort][_eq]=2526&filter[squads][squad_id][tribe][name][_eq]=FDND%20Jaar%202`);
-// const membersResponse = await fetch (`https://fdnd.directus.app/items/person?fields=*,squads.squad_id.name,squads.squad_id.cohort,squads.squad_id.tribe.name&filter[squads][squad_id][cohort][_eq]=2526&filter[squads][squad_id][tribe][name][_eq]=FDND%20Jaar%202&limit=20&page=0&_sort=jaar`);
 try {
 
 const personResponse = await fetch(`https://fdnd.directus.app/items/person/25`)
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,13 +1,84 @@
 <script>
-
-//   let { data } = $props();
-export let data;
+  export let data;
   const person = data.person;
-
 </script>
 
-{#if person}
-    <p>{person.name}</p>
-{:else}
-  <p>Lid niet gevonden</p>
-{/if}
+<main>
+
+<section>
+
+    <div class="lines">
+        <div class="line">
+        </div>
+        <div class="line">
+        </div>
+        <div class="line">
+        </div>
+    </div>
+
+    <img src="{person.avatar}" alt="Avatar van {person.name}" class="profile-img" />
+        <h1 class="text-block">{person.name}</h1>
+
+
+</section>
+
+</main>
+
+<style>
+
+    /* kleurtjes */
+    /* brown */
+    /* lightgrey */
+    /* white */
+
+
+main {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh; 
+}    
+
+section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    /* justify-content: center; */
+    height: 600px;
+    width: 300px;
+    margin: 0 auto;
+    border: 2px solid black;
+    border-radius: 20px;
+    background-color: beige;
+}    
+
+.lines {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    gap: 10px;
+    padding-top: 20px;
+    padding-bottom: 60px;
+}  
+
+.line {
+    width: 80px;
+    border: 2px solid black;
+}
+
+.profile-img {
+    object-fit: cover;
+    height: 300px;
+}
+
+.text-block {
+    margin-top: 100px;
+    width: 200px;
+    height: 30px;
+    border-radius: 20px;
+    border: 2px solid black;
+    background-color: lightgrey;
+    text-align: center;
+}
+
+</style>
